08d555ca6630456d02d5219698861fd5c908a62b
padgers <padgers@users.noreply.github.com>
2017-06-05 14:17:18 +0100

    Test-Something

---
+++ code/Test-Something.ps1
@@ -0,0 +1,14 @@
+Function Test-Something{
+        [cmdletbinding()]
+    Param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [string]$Test
+    )
+
+    Write-Output $Test
+
+
+}
